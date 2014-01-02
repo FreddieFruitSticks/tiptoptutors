8f78af0e3c5ee58f1671a9f458d21fe6e25b7b11
Darryl <darren@symfony.co.za>
2014-01-02 15:32:54 +0200

    Initial setup

--- apps/common/urls.py
+++ apps/common/urls.py
@@ -3,4 +3,5 @@ from common import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.HomeView.as_view() ,name='home'),
+                       url(r'^about/$', views.AboutView.as_view() ,name='about'),
                        )
--- apps/common/views.py
+++ apps/common/views.py
@@ -3,5 +3,5 @@ from django.views.generic import TemplateView
 class HomeView(TemplateView):
     template_name = "common/index.html"
 
-# class AboutView(TemplateView):
-#     template_name = "common/about.html"
+class AboutView(TemplateView):
+    template_name = "common/about.html"
