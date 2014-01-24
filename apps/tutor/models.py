2535cc7233120c6bd4e7b3761c77816c96c4f6a0
Darryl <darren@symfony.co.za>
2014-01-24 18:28:43 +0200

    working on forms

--- apps/tutor/models.py
+++ apps/tutor/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Tutor(models.Model):
     name = models.CharField(max_length=200, blank=False, null=False)
     email = models.EmailField(max_length=255, blank=False, null=False)
+
     created_at = models.DateTimeField(auto_now=True)
 
     def __unicode__(self):
