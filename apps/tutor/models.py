a3dc19d0d9280e9a8ba7cc9608263dd05a4804ef
Freddie <freddie@freddie-VirtualBox.(none)>
2014-03-02 18:12:26 +0200

    Changes

--- apps/tutor/models.py
+++ apps/tutor/models.py
@@ -10,13 +10,16 @@ class Tutor(models.Model):
     lesson      = models.CharField(verbose_name='lessons remaining', max_length=20)
     comment     = models.TextField()
     #front end display
-    name        = models.CharField(max_length=20)
+    name        = models.CharField(max_length=20, verbose_name="first name")
     surname     = models.CharField(max_length=20)
-    mobile      = models.CharField(max_length=10)
     email       = models.EmailField(max_length=25)
+    mobile      = models.CharField(max_length=10)
+    id_passport = models.CharField(max_length=20, verbose_name="id/passport number")
     subject     = models.ManyToManyField(AvailableTutorSubject)
+    transport   = models.BooleanField(verbose_name="own transport?")
+    id_doc      = models.FileField(verbose_name="id", upload_to="media/cv")
     cv          = models.FileField(blank=True, null=True, upload_to="media/cv")
-    academic    = models.FileField(blank=True, null=True, upload_to="media/academic/")
+    academic    = models.FileField(verbose_name="academic transcript",blank=True, null=True, upload_to="media/academic/")
 
     created_at = models.DateTimeField(auto_now=True)
 
