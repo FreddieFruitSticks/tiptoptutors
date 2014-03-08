13c54e73f43b7e10299e0bd23cfad6423bc567e5
Rizmari Versfeld <rizmari@praekelt.com>
2014-03-08 18:23:31 +0200

    add migration dependency

--- apps/student/migrations/0001_initial.py
+++ apps/student/migrations/0001_initial.py
@@ -7,6 +7,10 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ("option", "0001_initial"),
+    )
+
     def forwards(self, orm):
         # Adding model 'Student'
         db.create_table(u'student_student', (
