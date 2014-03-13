c58effa13543d3e767b22fa51f2a4e7ea7dc915a
Rizmari Versfeld <rizmari@praekelt.com>
2014-03-13 21:57:45 +0200

    finish pupil migration

--- apps/pupil/models.py
+++ apps/pupil/models.py
@@ -4,14 +4,14 @@ from tutor.models import Tutor
 
 
 class Pupil(models.Model):
-    tutor           = models.ForeignKey(Tutor)
+    tutor           = models.ForeignKey(Tutor, null=True, blank=True)
     name            = models.CharField(max_length=20)
     surname         = models.CharField(max_length=20)
     email           = models.EmailField()
     contact_number  = models.CharField(max_length=15)
     subject         = models.ManyToManyField(AvailableTutorSubject)
     level_of_study  = models.ForeignKey(LevelOfStudy)
-    street          = models.CharField(max_length=20, verbose_name=" address")
+    street          = models.CharField(max_length=20)
     suburb          = models.CharField(max_length=20)
     city            = models.ForeignKey(City)
     # available_times = models.TextField(blank=True, null=True)
@@ -20,4 +20,4 @@ class Pupil(models.Model):
     created_at = models.DateTimeField(auto_now=True)
 
     def __unicode__(self):
-        return self.name
+        return '%s %s' % (self.name, self.surname)
