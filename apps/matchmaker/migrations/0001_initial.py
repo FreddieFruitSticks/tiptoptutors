039534fb83cfa1dc9e5daedd1664044084d05611
Rizmari Versfeld <rizmari@praekelt.com>
2014-03-23 23:53:24 +0200

    initial models for tutor requests

--- apps/matchmaker/migrations/0001_initial.py
+++ apps/matchmaker/migrations/0001_initial.py
@@ -8,13 +8,126 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        pass
+        # Adding model 'RequestForTutor'
+        db.create_table(u'matchmaker_requestfortutor', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('pupil', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['pupil.Pupil'])),
+            ('status', self.gf('django.db.models.fields.CharField')(default='active', max_length=8)),
+            ('code', self.gf('django.db.models.fields.CharField')(unique=True, max_length=12)),
+            ('created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+        ))
+        db.send_create_signal(u'matchmaker', ['RequestForTutor'])
+
+        # Adding M2M table for field subjects on 'RequestForTutor'
+        m2m_table_name = db.shorten_name(u'matchmaker_requestfortutor_subjects')
+        db.create_table(m2m_table_name, (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('requestfortutor', models.ForeignKey(orm[u'matchmaker.requestfortutor'], null=False)),
+            ('availabletutorsubject', models.ForeignKey(orm[u'option.availabletutorsubject'], null=False))
+        ))
+        db.create_unique(m2m_table_name, ['requestfortutor_id', 'availabletutorsubject_id'])
+
+        # Adding model 'RequestSMS'
+        db.create_table(u'matchmaker_requestsms', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('request', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['matchmaker.RequestForTutor'])),
+            ('tutor', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['tutor.Tutor'])),
+            ('delivery_status', self.gf('django.db.models.fields.CharField')(default='unknown', max_length=16)),
+            ('response_text', self.gf('django.db.models.fields.CharField')(max_length=32, null=True, blank=True)),
+            ('response_timestamp', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
+            ('created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+        ))
+        db.send_create_signal(u'matchmaker', ['RequestSMS'])
+
 
     def backwards(self, orm):
-        pass
+        # Deleting model 'RequestForTutor'
+        db.delete_table(u'matchmaker_requestfortutor')
+
+        # Removing M2M table for field subjects on 'RequestForTutor'
+        db.delete_table(db.shorten_name(u'matchmaker_requestfortutor_subjects'))
+
+        # Deleting model 'RequestSMS'
+        db.delete_table(u'matchmaker_requestsms')
+
 
     models = {
-        
+        u'matchmaker.requestfortutor': {
+            'Meta': {'object_name': 'RequestForTutor'},
+            'code': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '12'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'pupil': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['pupil.Pupil']"}),
+            'status': ('django.db.models.fields.CharField', [], {'default': "'active'", 'max_length': '8'}),
+            'subjects': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['option.AvailableTutorSubject']", 'symmetrical': 'False'})
+        },
+        u'matchmaker.requestsms': {
+            'Meta': {'object_name': 'RequestSMS'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'delivery_status': ('django.db.models.fields.CharField', [], {'default': "'unknown'", 'max_length': '16'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'request': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['matchmaker.RequestForTutor']"}),
+            'response_text': ('django.db.models.fields.CharField', [], {'max_length': '32', 'null': 'True', 'blank': 'True'}),
+            'response_timestamp': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'tutor': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['tutor.Tutor']"})
+        },
+        u'option.availabletutorsubject': {
+            'Meta': {'ordering': "('name',)", 'object_name': 'AvailableTutorSubject'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        u'option.city': {
+            'Meta': {'object_name': 'City'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        u'option.levelofstudy': {
+            'Meta': {'object_name': 'LevelOfStudy'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        u'pupil.pupil': {
+            'Meta': {'object_name': 'Pupil'},
+            'city': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['option.City']"}),
+            'contact_number': ('django.db.models.fields.CharField', [], {'max_length': '15'}),
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'level_of_study': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['option.LevelOfStudy']"}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'requirement': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'street': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'subject': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['option.AvailableTutorSubject']", 'symmetrical': 'False'}),
+            'suburb': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'surname': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'tutor': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': u"orm['tutor.Tutor']", 'null': 'True', 'through': u"orm['pupil.PupilTutorMatch']", 'blank': 'True'})
+        },
+        u'pupil.pupiltutormatch': {
+            'Meta': {'object_name': 'PupilTutorMatch'},
+            'end_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'pupil': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['pupil.Pupil']"}),
+            'start_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'subject': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['option.AvailableTutorSubject']", 'symmetrical': 'False'}),
+            'tutor': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['tutor.Tutor']"})
+        },
+        u'tutor.tutor': {
+            'Meta': {'object_name': 'Tutor'},
+            'academic': ('django.db.models.fields.files.FileField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'comment': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'cv': ('django.db.models.fields.files.FileField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '25'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'id_doc': ('django.db.models.fields.files.FileField', [], {'max_length': '100'}),
+            'id_passport': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'mobile': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'status': ('django.db.models.fields.CharField', [], {'default': "'2'", 'max_length': '10'}),
+            'subject': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['option.AvailableTutorSubject']", 'symmetrical': 'False'}),
+            'surname': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'transport': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        }
     }
 
     complete_apps = ['matchmaker']
--- apps/matchmaker/models.py
+++ apps/matchmaker/models.py
@@ -1,7 +1,16 @@
+import random
+
 from django.db import models
 
 from pupil.models import Pupil
 from tutor.models import Tutor
+from option.models import AvailableTutorSubject
+
+
+# numbers + lowercase + uppercase
+REQUEST_CODE_CHARSET = [chr(i) for i in range(ord('0'), ord('9') + 1)] + \
+                       [chr(i) for i in range(ord('a'), ord('z') + 1)] + \
+                       [chr(i) for i in range(ord('A'), ord('Z') + 1)]
 
 
 class SubjectManager(models.Manager):
@@ -50,3 +59,51 @@ class TutorProxy(Tutor, SubjectMixin):
 
     def __unicode__(self):
         return '%s %s' % (self.name, self.surname)
+
+
+class RequestForTutor(models.Model):
+    '''
+    This is the base class for tutor requests. In future, multiple
+    communication media might be used to reach out to tutors. Only
+    generic functionality should go here.
+    '''
+    pupil = models.ForeignKey(PupilProxy)
+    subjects = models.ManyToManyField(AvailableTutorSubject)
+    # active: initial state, tutors can apply for this matchup
+    # inactive: probably after a tutor has been selected, applications
+    # will be ignored
+    status = models.CharField(
+        max_length=8,
+        default='active',
+        choices=(('active', 'Active'),
+                 ('inactive', 'Inactive'))
+    )
+    code = models.CharField(unique=True, max_length=12)
+    created = models.DateTimeField(auto_now_add=True)
+
+    @staticmethod
+    def generate_unique_code(length=12, charset=REQUEST_CODE_CHARSET):
+        code = None
+        while not code:
+            code = ''.join(random.choice(charset) for i in range(length))
+            if RequestForTutor.objects.filter(code=code).exists():
+                code = None
+        return code
+
+    def save(self, *args, **kwargs):
+        if not self.pk and not self.code:
+            self.code = self.generate_unique_code()
+        super(RequestForTutor, self).save(*args, **kwargs)
+
+
+class RequestSMS(models.Model):
+    request = models.ForeignKey(RequestForTutor)
+    tutor = models.ForeignKey(TutorProxy)
+    delivery_status = models.CharField(max_length=16, default='unknown')
+    response_text = models.CharField(max_length=32, null=True, blank=True)
+    response_timestamp = models.DateTimeField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def code(self):
+        return request.code
