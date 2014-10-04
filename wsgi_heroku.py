6c2e697a95c9dacf13326b1105b7530e9f7f59a3
Rizmari Versfeld <rizziepit@gmail.com>
2014-10-04 18:40:43 +0200

    hook up newrelic config file

--- wsgi_heroku.py
+++ wsgi_heroku.py
@@ -10,6 +10,12 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings_heroku")
 
+# newrelic setup
+import newrelic.agent
+os.environ.setdefault("NEW_RELIC_CONFIG_FILE", "newrelic.ini")
+newrelic.agent.initialize(os.environ["NEW_RELIC_CONFIG_FILE"])
+
+
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 application = Cling(get_wsgi_application())
