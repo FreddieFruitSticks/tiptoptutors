835b3365d486eaf9f4063de216ab951480a75e41
freddie <freddieodonnell@gmail.com>
2016-03-01 19:45:27 +0200

    removing static files dirs

--- settings_heroku.py
+++ settings_heroku.py
@@ -18,9 +18,9 @@ ALLOWED_HOSTS = ['*']
 # Static asset configuration
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, 'static'),
+# )
 
 INSTALLED_APPS += ('gunicorn', )
 
