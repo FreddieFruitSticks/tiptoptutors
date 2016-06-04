b03ebf656223cce7b2a25d8720959fec67b142e3
freddie <freddieodonnell@gmail.com>
2016-06-04 17:35:32 +0200

    hack in migration

--- apps/tutor/migrations/0006_auto_20160427_1505.py
+++ apps/tutor/migrations/0006_auto_20160427_1505.py
@@ -15,7 +15,7 @@ def set_user_on_tutor(apps, schema_editor):
             user = users.objects.get(email=tutor.email)
         except get_user_model().DoesNotExist:
             user = None
-        print 'user: ', user
+        print 'user: ', user.first_name, user.last_name, user.email
         tutor.user = user
         tutor.save()
 
