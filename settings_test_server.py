662559c0103d7f8063699ff421f74c170f00b6b6
freddie <freddieodonnell@gmail.com>
2016-06-13 14:06:54 +0200

    testing some https settings

--- settings_test_server.py
+++ settings_test_server.py
@@ -12,10 +12,10 @@ DATABASES['default'] = dj_database_url.config()
 # Honor the 'X-Forwarded-Proto' header for request.is_secure(). These are all security settings to ensure sending over
 # https instead of http.
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-# SECURE_SSL_REDIRECT = True
-# SESSION_COOKIE_SECURE = True
-# CSRF_COOKIE_SECURE = True
-# SECURE_HSTS_SECONDS = 60
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 60
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
